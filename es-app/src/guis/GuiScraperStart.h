478c254d231858807f5c4845be93627c4f1a769e
John Lioneil Dionisio <john.dionisio1@gmail.com>
2024-04-02 01:36:12 +0800

    fix: add helpstyle to scraper gui

--- es-app/src/guis/GuiScraperStart.h
+++ es-app/src/guis/GuiScraperStart.h
@@ -24,6 +24,7 @@ public:
 	bool input(InputConfig* config, Input input) override;
 
 	virtual std::vector<HelpPrompt> getHelpPrompts() override;
+	HelpStyle getHelpStyle() override;
 
 private:
 	void pressedStart();
